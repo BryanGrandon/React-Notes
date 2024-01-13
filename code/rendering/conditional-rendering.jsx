2b12021f3a910e2b458bd175d0f2c986c55ecd47
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-01-13 19:09:16 -0300

    feat(web):remove rendering folder

--- code/rendering/conditional-rendering.jsx
+++
@@ -1,8 +0,0 @@
-const getFoodIf = (isVegetarian) => {
-  if (isVegetarian) return "tofu";
-  else return "fish";
-};
-
-const getFoodTernary = (isVegetarian) => {
-  return isVegetarian ? "tofu" : "fish";
-};
