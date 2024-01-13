c68799649263bd5e776a1a75dac76517bd6a5c55
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-01-13 20:34:47 -0300

    feat(web):add hooks.jsx in the code folder and remove hooks folder

---
+++ code/hooks.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Counter = () => {
+  const [message, setMessage] = useState();
+  return (
+    <>
+      <input type="text" onChange={(e) => setMessage(e.target.value)} />
+      <button
+        onClick={() => {
+          alert(`The message is: ${message}`);
+        }}
+      >
+        Save
+      </button>
+    </>
+  );
+};
+
+root.render(<Counter />);
